d9b465236576a3dd3c7bbed79c6dd15ab789ad43
dfthan <varahahmo1@gmail.com>
2022-04-08 15:53:59 +0300

    2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,72 +1,76 @@
-import axios from 'axios'
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const SearchBar = ({ search, searchChange }) => {
-  return (
-    <div>
-      find countries <input value={search} onChange={searchChange} />
-    </div>
-  )
-}
-
-const Results = ({ results }) => {
-  console.log("tassa",results)
-  return (
-    <div>
-      <p></p>
-    </div>
-  )
-}
-
-/*
-const Result = ({ result }) => {
-  return (
-    <div>
-      <h1>{result.name}</h1>
-      <div>capital {result.capital}</div>
-      <div>population {result.population}</div>
+const Limit = ({ selectedCountries, setSelectedCountries }) => {
+  if (selectedCountries.length === 1) {
+    return (
+      <RenderResult result={selectedCountries[0]} />
+    )
+  }
+  else if (selectedCountries.length <= 10 && selectedCountries.length > 1) {
+    return (
       <div>
-        <h3>Spoken languages</h3>
-        <ul>
-          {result.languages.map(
-            language => <li key={language.name}>{language.name}</li>
-          )}
-        </ul>
-        <div><img alt={`${result.name} flag`} src={result.flag} /></div>
+        {selectedCountries.map(name => (
+          <ol key={name}>
+            {name.name}
+            <button onClick={()=> setSelectedCountries([name])}>show</button>
+          </ol>
+        ))}
       </div>
+    )
+  }
+  else {
+    return (
+      <p>Too many matches, specify another filter</p>
+    )
+  }
+
+}
+
+const RenderResult = ({ result }) => {
+  return (
+    <div>
+      <h2>{result.name}</h2>
+      <div>
+        capital: {result.capital}
+      </div>
+      <div>
+        area: {result.area}
+      </div>
+      <h3>languages:</h3>
+      <ul>
+        {result.languages.map(({ name }) => <li>{name}</li>)}
+      </ul>
+      <img alt="Countrys flag" src={result.flag} width="100" />
+
     </div>
   )
 }
-*/ 
 
 function App() {
   const [results, setResults] = useState([])
-  const [search, setSearch] = useState("")
+  const [selectedCountries, setSelectedCountries] = useState([])
 
   useEffect(() => {
     axios
-      .get("https://restcountries.com/v3.1/all")
+      .get("https://restcountries.com/v2/all")
       .then(dataResponse => {
         setResults(dataResponse.data)
       })
   }, [])
+  console.log("length: ", results.length)
 
 
-  //const countriesFiltered = results.filter(result => result.includes(search))
-
-  const searchChange = (event) => {
-    setSearch(event.target.value)
-    console.log(event.target.value)
-    var testi = results.filter(results => results.name === "Finland")
-    console.log("123", testi)
+  const searchChange = event => {
+    const resultsFiltered = results.filter(result => result.name.toLowerCase().includes(event.target.value.toLowerCase()))
+    setSelectedCountries(resultsFiltered)
   }
-
-
+  console.log(selectedCountries.length)
 
   return (
     <div>
-      <SearchBar search={search} searchChange={searchChange} />
-      <Results results={results} />
+      find countries <input onChange={searchChange} />
+      <Limit selectedCountries={selectedCountries} setSelectedCountries={setSelectedCountries} />
     </div>
   )
 }
